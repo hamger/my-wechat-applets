f0c3a69caff15da3da60ec867c48a381864758f5
hamger <2313303800@qq.com>
2019-01-15 19:29:31 +0800

    init2

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,15 +1,46 @@
-//logs.js
-const util = require('../../utils/util.js')
-
 Page({
   data: {
-    logs: []
+    logs: [],
+    modalHidden: true,
+    toastHidden: true
   },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
+  onShow: function() {
+    wx.setNavigationBarTitle({
+      title: '任务记录'
     })
+    this.getLogs()
+  },
+  set: function() {
+
+  },
+  getLogs: function() {
+    // wx.removeStorageSync('logs')
+    let logs = wx.getStorageSync('logs')
+    console.log(logs)
+    logs.forEach(function(item) {
+      item.startTime = new Date(item.startTime).toLocaleString()
+    })
+    this.setData({
+      logs: logs
+    })
+  },
+  onLoad: function() {},
+  switchModal: function() {
+    this.setData({
+      modalHidden: !this.data.modalHidden
+    })
+  },
+  hideToast: function() {
+    this.setData({
+      toastHidden: true
+    })
+  },
+  clearLog: function(e) {
+    wx.setStorageSync('logs', [])
+    this.switchModal()
+    this.setData({
+      toastHidden: false
+    })
+    this.getLogs()
   }
 })
